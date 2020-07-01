377295bf9a6e01521ad9ec91d2a50eeca7ba668e
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-07-01 15:16:30 +0900

    Add files via upload

---
+++ hellokotlin_2/src/com/example/edu/contorl.kt
@@ -0,0 +1,4 @@
+package com.example.edu
+//파일에 클래스가 하나 있고 그것의 이름이 파일과 같다면 확장자가 생략된다.
+class contorl(val args:String)
+class Test(val abc:String)
---
+++ hellokotlin_2/src/com/example/edu/person.kt
@@ -0,0 +1,4 @@
+package com.example.edu
+class person(val name:String, val  age:Int)
+//다른 패키지에서 사용하고 싶다면 import 사용
+//같은 이름 있다면 as로 이름 변경해 충돌을 피한다.
